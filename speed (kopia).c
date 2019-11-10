6030a76920ad847ef40bdbdca8c3060d47a13c23
Your Name <you@example.com>
2019-11-10 17:27:20 +0100

    komentarz

---
+++ speed (kopia).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+
+
+
+
+int main()
+{
+   
+
+    printf("Benchmarking variant %s\n", XMSS_VARIANT);
+
+    printf("Generating keypair.. ");
+
+    return 0;
+}
